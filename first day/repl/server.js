6435dab2d9c7d89b3f24326fb3999a60975b71ce
594237406 <hongji_12314@163.com>
2015-06-27 17:14:37 +0800

    Signed-off-by: 594237406 <hongji_12314@163.com>

---
+++ first day/repl/server.js
@@ -0,0 +1,41 @@
+/**
+ * Created by lihongji on 2015/6/27.
+ */
+
+
+var http=require("http");
+
+var server=http.createServer(waiter);
+
+function waiter(req,rsp){
+    rsp.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
+    var url=req.url;
+
+    var a=url.split("?");
+
+    var pathname=a[0];
+    console.log(pathname);
+    var params= a[1];
+
+
+    params=params.split("&");
+    var paramsObj={};
+    for(var i in params){
+        var param=params[i];
+        param=param.split("=");
+        paramsObj[param[0]]= param[1];
+    }
+
+
+    if(pathname=="/menu"){
+        rsp.end("<ul><li><a href='/chiken'>黄焖鸡</a></li><li><a href='/leg'>鸡腿饭</a></li></ul>");
+    }else if(pathname=="/leg"){
+        rsp.end("鸡腿饭");
+    }else if(pathname=="/chiken"){
+        rsp.end("黄焖鸡"+paramsObj["num"]+"份");
+    }else{
+        rsp.end(pathname);
+    }
+}
+
+server.listen(8080,"localhost");
